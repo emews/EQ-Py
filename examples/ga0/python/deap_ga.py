c457589e13abfa4d7d26a151d2f73f7169c1c9fb
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-11-02 15:32:15 -0500

    Override individual creation

--- examples/ga0/python/deap_ga.py
+++ examples/ga0/python/deap_ga.py
@@ -27,26 +27,25 @@ def obj_func(x):
     """ Dummy function for compatibility with DEAP """
     assert(False)
 
+def make_random_params():
+    def random_param():
+        return random.random() * 4 - 2
+    x1 = random_param()
+    x2 = random_param()
+    return [x1, x2]
+
 def create_list_of_lists_string(list_of_lists, super_delim=";", sub_delim=","):
     # super list elements separated by ;
-    res = []
+    L = []
     for x in list_of_lists:
-        res.append(sub_delim.join(str(n) for n in x))
-    return (super_delim.join(res))
-
-def create_fitnesses(params_string):
-    """return equivalent length tuple list
-    :type params_string: str
-    """
-    params = params_string.split(";")
-    # get length
-    res = [(i,) for i in range(len(params))]
-    return (res)
+        L.append(sub_delim.join(str(n) for n in x))
+    result = super_delim.join(L)
+    return result
 
 def queue_map(obj_func, pops):
-    # Note that the obj_func is not used
-    # sending data that looks like:
-    # [[a,b,c,d],[e,f,g,h],...]
+    """ Note that the obj_func is a dummy
+        pops: data that looks like: [[x1,x2],[x1,x2],...]
+    """
     if not pops:
         return []
     eqpy.OUT_put(create_list_of_lists_string(pops))
@@ -92,14 +91,16 @@ def run():
     creator.create("Individual", list, fitness=creator.FitnessMin)
     toolbox = base.Toolbox()
     toolbox.register("attr_float", random.random)
-    toolbox.register("individual", tools.initRepeat, creator.Individual,
-                     toolbox.attr_float, n=2)
+    # toolbox.register("individual", tools.initRepeat, creator.Individual,
+    #                 toolbox.attr_float, n=2)
+    toolbox.register("individual", tools.initIterate, creator.Individual,
+                     make_random_params)
 
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("evaluate", obj_func)
     toolbox.register("mate", cxUniform, indpb=mate_pb)
     toolbox.register("mutate", custom_mutate, indpb=mutate_pb)
-    toolbox.register("select", tools.selTournament, tournsize=3)
+    toolbox.register("select", tools.selTournament, tournsize=num_pop/2)
     toolbox.register("map", queue_map)
 
     pop = toolbox.population(n=num_pop)
