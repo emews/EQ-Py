34584f9f55758a2adfa4a5df1b0fd78c72272009
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-11-14 22:45:24 -0600

    Run script for Beagle

---
+++ examples/noop/beagle/run-swift.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+if [[ ${PROJECT:-} == "" ]]
+then
+  echo "Error: You need to set environment variable PROJECT."
+  exit 1
+fi
+
+THIS=$( cd $( dirname $0 ); /bin/pwd )
+
+PATH=/lustre/beagle2/wozniak/Public/sfw/swift-t/py2Lr/stc/bin:$PATH
+
+export LD_LIBRARY_PATH=
+LD_LIBRARY_PATH+=/opt/gcc/4.9.2/snos/lib64:
+LD_LIBRARY_PATH+=/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/lib
+
+export T_PROJECT_ROOT=$( cd $THIS/.. ; /bin/pwd )
+EQP=$T_PROJECT_ROOT/ext/EQ-Py
+
+export PYTHONPATH=$T_PROJECT_ROOT/python:$EQP
+export TURBINE_RESIDENT_WORK_WORKERS=1
+
+export TURBINE_OUTPUT_ROOT=$PWD
+export TURBINE_OUTPUT_FORMAT=out-%Q
+export WALLTIME=01:00:00
+PROCS=3
+swift-t -m cray -n $PROCS -p -I $EQP -r $EQP \
+        $T_PROJECT_ROOT/swift/workflow.swift
