75e51eee8c3a7b57db5f91bceb93a5edd667bf41
Justin M Wozniak <wozniak@mcs.anl.gov>
2019-03-21 09:42:52 -0500

    Readability improvements

--- examples/noop/swift/workflow.swift
+++ examples/noop/swift/workflow.swift
@@ -15,42 +15,43 @@ import EQPy;
 string emews_root = getenv("EMEWS_PROJECT_ROOT");
 string algo = argv("algorithm");
 
-(void v) loop(location loc)
+(void v) loop(location L)
 {
-  printf("Beginning loop.");
+  printf("Entering loop...");
   for (boolean b = true, int i = 1;
        b;
        b=c, i = i + 1)
   {
-    result = EQPy_get(loc);
-    printf("swift: result: %s", result);
+    result = EQPy_get(L);
+    printf("swift: from EQ/Py: %s", result);
     boolean c;
     if (result == "FINAL")
     {
-      printf("setting void") =>
+      printf("Exiting loop.") =>
         v = propagate() =>
         c = false;
     } else if (result == "EQPY_ABORT") {
-        string why = EQPy_get(loc);
+        string why = EQPy_get(L);
         printf("%s", why) =>
         v = propagate() =>
         c = false;
     }
     else
     {
-      data = fromint(toint(result) + 1);
-      printf("swift: data: %s", data);
-      EQPy_put(loc, data) => c = true;
+      data = int2string(string2int(result) + 1);
+      printf("swift: to EQ/Py:   %s", data);
+      EQPy_put(L, data) => c = true;
     }
   }
 
 }
 
 main() {
-  printf("WORKFLOW!");
+  printf("SWIFT WORKFLOW STARTING...");
 
   location L = locationFromRank(1);
   EQPy_init_package(L, algo) =>
   loop(L) =>
-  EQPy_stop(L);
+  EQPy_stop(L) =>
+    printf("SWIFT WORKFLOW COMPLETE");
 }
